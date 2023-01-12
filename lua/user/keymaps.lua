589b6af22dbdcdb36b805181c3f2a96605c2f2ba
wilson <wilsonsinclair6@gmail.com>
2023-01-12 14:43:43 -0600

    Initial Lua config commit

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,29 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = {silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--Normal --
+--Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+
+--TreeSitter
+keymap("n", "<C-t>", ":NvimTreeToggle<CR>", opts)
+
+--Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+
+
+
+
